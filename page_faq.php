255e84ffcbd6e8a69a537916cded75bdd3ca2786
MatheusOfficina <MatheusOfficina@users.noreply.github.com>
2020-11-14 12:02:32 -0300

    Create page_faq.php

---
+++ page_faq.php
@@ -0,0 +1,20 @@
+<?php /* Template Name: FAQ */ ?>
+<?php get_header(); ?>
+
+<section class="faq institutional__pages">
+    <div class="container">
+        <div class="breadcrumb__content">
+            <a href="#" class="breadcrumb__link">
+                Voltar à home
+            </a>
+        </div>
+        <div class="privacy_title d-flex justify-content-center">
+            <h2 class="title--primary">FAQ</h2>
+        </div>
+        <div class="row">
+           
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
