ff54e28cab1848fcfe7d939dd2b91d087d056b94
Mateus Monteiro <mateusmonteiroes@gmail.com>
2020-11-29 18:09:12 -0300

    Update form-function.php

--- inc/form-function.php
+++ inc/form-function.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 // add Imovel
 
 function monsi_admin_add_imovel()
@@ -12,70 +11,103 @@ function monsi_admin_add_imovel()
 
     if ($user_id > 0) {
 
-        $nome = sanitize_text_field($request['nome_do_imovel']);
-        $faixa_de_renda = sanitize_text_field($request['faixa_de_renda']);
-        $numeroprocesso = sanitize_text_field($request['numeroprocesso']);
+        $name_of_property = sanitize_text_field($request['name_of_property']);
+        $voluntary_self_inspection = sanitize_text_field($request['voluntary_self_inspection']);
+        $process_number = sanitize_text_field($request['process_number']);
+        $type_of_property = trim($request['type_of_property']);
         $iptu = sanitize_text_field($request['iptu']);
-        $cliente_light = sanitize_text_field($request['cliente_light']);
         $cep = sanitize_text_field($request['cep']);
-        $endereco = sanitize_text_field($request['endereco']);
-        $valor_da_ultima_conta = sanitize_text_field($request['valor_da_ultima_conta']);
-        $mes_da_conta = sanitize_text_field($request['mes_da_conta']);
-        $consumo_total_anual = sanitize_text_field($request['consumo_total_anual']);
-        $tarifa_de_energia = sanitize_text_field($request['tarifa_de_energia']);
-        $qual_tipo_predominante_de_ar_condicionado = sanitize_text_field($request['qual_tipo_predominante_de_ar_condicionado']);
-        $qual_o_tipo_de_iluminacao_predominante = sanitize_text_field($request['qual_o_tipo_de_iluminacao_predominante']);
-        $area_total_disponivel_na_coberturatelhados = sanitize_text_field($request['area_total_disponivel_na_coberturatelhados']);
-
-        $qual_o_tipo_de_uso_de_imovel = sanitize_text_field($request['qual_o_tipo_de_uso_de_imovel']);
-        $possui_paineis_fotovoltaicos = sanitize_text_field($request['possui_paineis_fotovoltaicos']);
-        $voluntario_autovistoria = sanitize_text_field($request['voluntario_autovistoria']);
-
-        $area_total_disponivel_na_coberturatelhados = sanitize_text_field($request['area_total_disponivel_na_coberturatelhados']);
-        $possui_paineis_solares_para_aquecimento_de_agua = sanitize_text_field($request['possui_paineis_solares_para_aquecimento_de_agua']);
-        $qual_o_tipo_de_iluminacao_predominante = sanitize_text_field($request['qual_o_tipo_de_iluminacao_predominante']);
-        $idade_media_dos_equipamentos_de_ar_condicionado = sanitize_text_field($request['idade_media_dos_equipamentos_de_ar_condicionado']);
-        $nao_possuo_ar = sanitize_text_field($request['nao_possuo_ar']);
-        $tipoimovel = trim($request['type_of_property']);
+        $address = sanitize_text_field($request['address']);
+        $client_light = sanitize_text_field($request['client_light']);
+        $income_range = sanitize_text_field($request['income_range']);
+        $which_residence_type = sanitize_text_field($request['which_residence_type']);
+        $total_area = sanitize_text_field($request['total_area']);
+        $mixed_type_property = sanitize_text_field($request['mixed_type_property']);
+        $total_place_area = sanitize_text_field($request['total_place_area']);
+        $total_conditioned_area = sanitize_text_field($request['total_conditioned_area']);
+        $number_of_floors = sanitize_text_field($request['number_of_floors']);
+        $occupants_useful_day = sanitize_text_field($request['occupants_useful_day']);
+        $working_hours = sanitize_text_field($request['working_hours']);
+        $percentage_occupancy = sanitize_text_field($request['percentage_occupancy']);
+        $what_property_use = sanitize_text_field($request['what_property_use']);
+        $value_last_account = sanitize_text_field($request['value_last_account']);
+        $account_month = sanitize_text_field($request['account_month']);
+        $total_annual_consumption = sanitize_text_field($request['total_annual_consumption']);
+        $energy_tariff = sanitize_text_field($request['energy_tariff']);
+        $consumption_data = sanitize_text_field($request['consumption_data']);
+        $last_energy_bill = sanitize_text_field($request['last_energy_bill']);
+        $account_month_a1 = sanitize_text_field($request['account_month_a1']);
+        $hired_demand = sanitize_text_field($request['hired_demand']);
+        $rush_hour_consumption = sanitize_text_field($request['rush_hour_consumption']);
+        $off_time_consumption = sanitize_text_field($request['off_time_consumption']);
+        $last_account_value = sanitize_text_field($request['last_account_value']);
+        $month_of_invoice = sanitize_text_field($request['month_of_invoice']);
+        $predominant_air_conditioning = sanitize_text_field($request['predominant_air_conditioning']);
+        $air_age_equipment = sanitize_text_field($request['air_age_equipment']);
+        $automation_system = sanitize_text_field($request['automation_system']);
+        $on_off_system = sanitize_text_field($request['on_off_system']);
+        $temperature_control = sanitize_text_field($request['temperature_control']);
+        $do_not_have = sanitize_text_field($request['do_not_have']);
+        $predominant_type_illumination = sanitize_text_field($request['predominant_type_illumination']);
+        $has_photovoltaic_panels = sanitize_text_field($request['has_photovoltaic_panels']);
+        $panels_water_heating = sanitize_text_field($request['panels_water_heating']);
+        $available_on_rooftops = sanitize_text_field($request['available_on_rooftops']);
 
 
         $response = array(
             'post_author' => $user_id,
             'post_type' => 'imoveis',
-            'post_title' => $nome,
+            'post_title' => $name_of_property,
             'post_status' => 'publish',
-			      'tax_input'    => array($tipoimovel),
+            'tax_input' => array($type_of_property),
             'meta_input' => array(
-				    'tipoimovel' => $tipoimovel,
-                'nome_do_imovel' => $nome,
-                'o_preenchimento_e_voluntario_ou_autovistoria' => $voluntario_autovistoria,
-                'numero_do_processo' => $numeroprocesso,
+                'type_of_property' => $type_of_property,
+                'post_name' => $name_of_property,
+                'voluntary_self_inspection' => $voluntary_self_inspection,
+                'process_number' => $process_number,
                 'iptu' => $iptu,
                 'cep' => $cep,
-                'endereco' => $endereco,
-                'cliente_light' => $cliente_light,
-                'faixa_de_renda' => $faixa_de_renda,
-                'qual_o_tipo_da_residencia' => '',
-                'area_total' => '',
-                'e_misto_comercial_+_residencial' => '',
-                'qual_o_tipo_de_uso_de_imovel' => $qual_o_tipo_de_uso_de_imovel,
-                'valor_da_ultima_conta' => $valor_da_ultima_conta,
-                'mes_da_conta' => $mes_da_conta,
-                'consumo_total_anual' => $consumo_total_anual,
-                'tarifa_de_energia' => $tarifa_de_energia,
-                'qual_tipo_predominante_de_ar_condicionado' => $qual_tipo_predominante_de_ar_condicionado,
-                'idade_media_dos_equipamentos_de_ar_condicionado' => $idade_media_dos_equipamentos_de_ar_condicionado,
-                'nao_possuo_ar' => $nao_possuo_ar,
-                'qual_o_tipo_de_iluminacao_predominante' => $qual_o_tipo_de_iluminacao_predominante,
-                'possui_paineis_fotovoltaicos' => $possui_paineis_fotovoltaicos,
-                'possui_paineis_solares_para_aquecimento_de_agua' => $possui_paineis_solares_para_aquecimento_de_agua,
-                'area_total_disponivel_na_coberturatelhados' => $area_total_disponivel_na_coberturatelhados,
+                'address' => $address,
+                'client_light' => $client_light,
+                'income_range' => $income_range,
+                'which_residence_type' => $which_residence_type,
+                'total_area' => $total_area,
+                'mixed_type_property' => $mixed_type_property,
+                'total_place_area' => $total_place_area,
+                'total_conditioned_area' => $total_conditioned_area,
+                'number_of_floors' => $number_of_floors,
+                'occupants_useful_day' => $occupants_useful_day,
+                'working_hours' => $working_hours,
+                'percentage_occupancy' => $percentage_occupancy,
+                'what_property_use' => $what_property_use,
+                'value_last_account' => $value_last_account,
+                'account_month' => $account_month,
+                'total_annual_consumption' => $total_annual_consumption,
+                'energy_tariff' => $energy_tariff,
+                'consumption_data' => $consumption_data,
+                'last_energy_bill' => $last_energy_bill,
+                'account_month_a1' => $account_month_a1,
+                'hired_demand' => $hired_demand,
+                'rush_hour_consumption' => $rush_hour_consumption,
+                'off_time_consumption' => $off_time_consumption,
+                'last_account_value' => $last_account_value,
+                'month_of_invoice' => $month_of_invoice,
+                'predominant_air_conditioning' => $predominant_air_conditioning,
+                'air_age_equipment' => $air_age_equipment,
+                'automation_system' => $automation_system,
+                'on_off_system' => $on_off_system,
+                'temperature_control' => $temperature_control,
+                'do_not_have' => $do_not_have,
+                'predominant_type_illumination' => $predominant_type_illumination,
+                'has_photovoltaic_panels' => $has_photovoltaic_panels,
+                'panels_water_heating' => $panels_water_heating,
+                'available_on_rooftops' => $available_on_rooftops
             ),
         );
 
         var_dump($request);
         $post_id = wp_insert_post($response);
-        wp_set_post_terms( $post_id, $tipoimovel, 'tipos_de_imoveis' ); 
+        wp_set_post_terms($post_id, $type_of_property, 'tipos_de_imoveis');
     }
 }
 
